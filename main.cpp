a792c4c4ca4ae6a5ed1cfe68e59ac36d15363db6
PGG106 <g.pagano45@studenti.unisa.it>
2023-05-05 10:30:49 +0200

    fully loop for simd, add benching code

--- main.cpp
+++ main.cpp
@@ -1,88 +1,71 @@
-#include <cstdint>
-#include <chrono>
 #include <iostream>
 #include <array>
 #include <immintrin.h>
-/*AVX512 - VNNI INTRODUCES 4 NEW INSTRUCTIONS
-* VPDPBUSD, VPDPBUSDS, VPDPWSSD, VPDPWSSDS
-* VPDPBUSD AND VPDPWSSD WORK IDENTICALLY TO VPDPBUSDS AND VPDPWSSDS BUT THE LATTER ARE SATURETED
-* FOR BENCHMARKING PURPOSES WE CAN FOCUS ON JUST VPDPBUSD AND VPDPWSSD
-*
-* VPDPBUSD - Multiplies the individual bytes (8-bit) of the first source operand by the corresponding bytes (8-bit) of the second source operand,
-producing intermediate word (16-bit) results which are summed and accumulated in the double word (32-bit) of the destination operand
-*
-* Multiplies the individual words (16-bit) of the first source operand by the corresponding word (16-bit) of the second source operand,
-producing intermediate word results which are summed and accumulated in the double word (32-bit) of the destination operand
-*
-*
-*
-*/
-template <typename T>
-void print_register(__m512i r) {
-	constexpr auto entries_count1 = 512 / (8 * sizeof(T));
-	std::array<T, entries_count1> values = {};
-	std::cout << "entries count is: " << entries_count1 << "\n";
-	_mm512_storeu_epi32(values.data(), r);
-	std::cout << +values[0] << std::endl;
+#include <chrono>
 
+template<typename T>
+void print_register(const __m512i& zmm0)
+{
+	constexpr auto entries_count = 512 / (8 * sizeof(T));
+
+	std::array<T, entries_count> values = {};
+	_mm512_storeu_si512((__m512i*)values.data(), zmm0);
+
+	for (size_t i = 0; i < entries_count; i++) {
+		std::cout << static_cast<int64_t>(values[i]) << "\n";
+	}
 }
+
+
 int main()
 {
-	/*
-	VPDPBUSD offers 3 intrinsics for 512 bits wide registers:
-	_mm512_dpbusd_epi32
-	_mm512_mask_dpbusd_epi32
-	_mm512_maskz_dpbusd_epi32
+	std::array<uint8_t, 1024> src1 = {};
+	std::array<int8_t, 1024> src2 = {};
+	std::array<int32_t, 256> src3 = {};
+	std::array<int32_t, 256> dst1 = {};
 
-	the main action all 3 intrinsics do is:
-	Multiply groups of 4 adjacent pairs of unsigned 8-bit integers in a with corresponding signed 8-bit integers in b, producing 4 intermediate signed 16-bit results.
-	Sum these 4 results with the corresponding 32-bit integer in src, and store the packed 32-bit results in dst.
-
-	_mm512_mask_dpbusd_epi32 and _mm512_maskz_dpbusd_epi32 have additional uses of a write and zero mask respectively we don't care about from a performance standpoint
-
-	*/
-
-	//We start off by creating the arrays to manipulate with the istructions
-	std::array<std::uint8_t, 1024> src1 = {};
-	std::array<std::int8_t, 1024> src2 = {};
-	std::array<std::int32_t, 256> src3 = {};
-	std::array<std::int32_t, 256> dst = {};
-
-	for (int i = 0;i < 1024;i++) {
-		src1[i] = i % 240;
-		src2[i] = i % 100;
+	for (size_t i = 0; i < 1024; i++) {
+		src1[i] = static_cast<uint8_t>(i % 240);
+		src2[i] = static_cast<int8_t>(i % 100);
 	}
-	for (int i = 0;i < 256;i++) {
-		src3[i] = i;
+
+	for (size_t i = 0; i < 256; i++) {
+		src3[i] = static_cast<int32_t>(i);
 	}
-	//Mimick the desired end result without using intrinsics
-	for (int i = 0;i < 256;i++) {
-		dst[i] = (src1[i * 4] * src2[i * 4]) +
+	auto start = std::chrono::high_resolution_clock::now();
+	for (size_t i = 0; i < 256; i++) {
+		dst1[i] = static_cast<int32_t>(
+			(src1[i * 4] * src2[i * 4]) +
 			(src1[i * 4 + 1] * src2[i * 4 + 1]) +
 			(src1[i * 4 + 2] * src2[i * 4 + 2]) +
 			(src1[i * 4 + 3] * src2[i * 4 + 3]) +
-			src3[i];
+			src3[i]
+			);
 	}
-	/**************************************** AVX512VNNI intrisics ****************************/
+	auto stop = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count();
+	std::cout << "Autovec code took " << duration << " nanoseconds" << std::endl;
+	for (size_t i = 240; i < 256; i++) {
+		std::cout << static_cast<int64_t>(dst1[i]) << "\n";
+	}
+
+	std::cout << " --- \n";
 #if defined(__AVX512VNNI__) && defined(__AVX512F__)
-	std::cout << "Now trying AVX512VNNI intrinsics\n";
-	//The goal is doing dst = (src*src2) + src3 with 1 instruction
-	//Create the registers to load the arrays into
+	std::cout << "Now trying AVX512VNNI instrisics\n";
 	__m512i _src1, _src2, _src3, _dst;
-	//Load data in the arrays
-	_src1 = _mm512_loadu_epi8(src1);
-	_src2 = _mm512_loadu_epi8(src2);
-	_src3 = _mm512_loadu_epi32(src3);
-	_dst = _mm512_dpbusd_epi32(_src3, _src1, _src2);
-
-	print_register<std::uint8_t>(_src1);
-	print_register<std::int8_t>(_src2);
-	print_register<std::int32_t>(_src3);
-	print_register<std::int32_t>(_dst);
-
+	auto simd_start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < (1024 / 64);i++) {
+		_src1 = _mm512_loadu_si512((__m512i const*)&src1[64 * i]);
+		_src2 = _mm512_loadu_si512((__m512i const*)&src2[64 * i]);
+		_src3 = _mm512_loadu_si512((__m512i const*)&src3[16 * i]);
+		_dst = _mm512_dpbusd_epi32(_src3, _src1, _src2);
+	}
+	auto simd_stop = std::chrono::high_resolution_clock::now();
+	auto simd_duration = std::chrono::duration_cast<std::chrono::nanoseconds>(simd_stop - simd_start).count();
+	std::cout << "SIMD code took " << simd_duration << " nanoseconds" << std::endl;
+	print_register<int32_t>(_dst);
 #else
 	std::cout << " AVX512VNNI Support not found\n";
 #endif
 	return 0;
 }
-
