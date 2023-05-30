29fbfe5668e1bd2036c42f60b6b65440ac3338fd
PGG106 <g.pagano45@studenti.unisa.it>
2023-05-30 03:55:53 +0200

    final fix for convolution code

--- main.cpp
+++ main.cpp
@@ -131,6 +131,7 @@ void convolute(std::array<std::array<int8_t, 4>, 4> input, std::array<std::array
 				// Going over every column of each row
 				for (int l = 0; l < kernel_columns; l++)
 				{
+					std::cout << "kernel[" << k << "]" << "[" << l << "]" << " = " << int(kernel[k][l]) << std::endl;
 					// Convolute input square with kernel square
 					convolute += input[x][y] * kernel[k][l];
 					y++; // Move right.
@@ -145,7 +146,7 @@ void convolute(std::array<std::array<int8_t, 4>, 4> input, std::array<std::array
 	}
 	auto stop = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count();
-	std::cout << duration << std::endl;
+	std::cout << "Autovec convolution took: " << duration << std::endl;
 	for (std::array<int, width> row : output) {
 		for (int element : row) {
 			std::cout << element << " ";
@@ -157,37 +158,15 @@ void convolute(std::array<std::array<int8_t, 4>, 4> input, std::array<std::array
 
 
 //Convolute an input matrix with a kernel and add a bias
+//Convolute an input matrix with a kernel and add a bias
 void convoluteSIMD(std::array<std::array<int8_t, 4>, 4> input, std::array<std::array<int8_t, 2>, 2> kernel, int32_t bias)
 {
-
-
-
-#if defined(__AVX512VNNI__) && defined(__AVX512F__)
 	std::cout << "Now trying AVX512VNNI instrisics\n";
 	__m512i _src1, _src2, _src3, _dst;
-	constexpr int register_size = 512;
-	constexpr int int8_per_register = 512 / 8;
-	constexpr int int32_per_register = 512 / 32;
-	total_time = 0;
-	//Loop 1000 times to get a good average
-	for (int j = 0;j < samples;j++) {
-		auto simd_start = std::chrono::high_resolution_clock::now();
-		for (int i = 0; i < 1024 / int8_per_register;i++) {
-			_src1 = _mm512_loadu_si512((__m512i const*)&src1[int8_per_register * i]);
-			_src2 = _mm512_loadu_si512((__m512i const*)&src2[int8_per_register * i]);
-			_src3 = _mm512_loadu_si512((__m512i const*)&src3[int32_per_register * i]);
-			_dst = _mm512_dpbusd_epi32(_src3, _src1, _src2);
-		}
-		auto simd_stop = std::chrono::high_resolution_clock::now();
-		auto simd_duration = std::chrono::duration_cast<std::chrono::nanoseconds>(simd_stop - simd_start).count();
-		total_time += simd_duration;
-	}
+	int convolutions[512];
+	int8_t src1[512];
+	int8_t src2[512];
 
-#endif
-
-
-	int convolute = 0; // This holds the convolution results for an index.
-	int x, y; // Used for input matrix index
 	constexpr int input_rows = 4, input_columns = 4, kernel_rows = 2, kernel_columns = 2;
 
 	//Size output array
@@ -195,46 +174,36 @@ void convoluteSIMD(std::array<std::array<int8_t, 4>, 4> input, std::array<std::a
 
 	const int height = (input_rows - kernel_rows) + 1;
 
-	std::array<std::array<int32_t, height>, width> output;
+	// Load the bias vector
+	_src3 = _mm512_set1_epi32(bias);
+	//Duplicate elements in the kernel vector 
+	_src2 = _mm512_set1_epi32((kernel[0][0]) + (kernel[0][1] << 8) + ((kernel[1][0] + 1) << 16) + ((kernel[1][1] + 1) << 24));
+	//Load the input vector in the exact order we need it in
+	_src1 = _mm512_set_epi32(
+		0, 0, 0, 0, 0, 0, 0,
+		input[2][2] + (input[2][3] << 8) + (input[3][2] << 16) + (input[3][3] << 24),
+		input[2][1] + (input[2][2] << 8) + (input[3][1] << 16) + (input[3][2] << 24),
+		input[2][0] + (input[2][1] << 8) + (input[3][0] << 16) + (input[3][1] << 24),
+		input[1][2] + (input[1][3] << 8) + (input[2][2] << 16) + (input[2][3] << 24),
+		input[1][1] + (input[1][2] << 8) + (input[2][1] << 16) + (input[2][2] << 24),
+		input[1][0] + (input[1][1] << 8) + (input[2][0] << 16) + (input[2][1] << 24),
+		input[0][2] + (input[0][3] << 8) + (input[1][2] << 16) + (input[1][3] << 24),
+		input[0][1] + (input[0][2] << 8) + (input[1][1] << 16) + (input[1][2] << 24),
+		input[0][0] + (input[0][1] << 8) + (input[1][0] << 16) + (input[1][1] << 24)
+	);
 
-	// Going over every row of the input
-	for (int i = 0; i < input_rows; i++)
-	{
-		// Going over every column of each row
-		for (int j = 0; j < input_columns; j++)
-		{
-			//Pinpot input value we are working on
-			x = i;
-			y = j;
-			//Quick check for if we are out of bounds
-			if (!(x + kernel_rows <= input_rows)) break;
-			if (!(y + kernel_columns <= input_columns)) break;
+	_dst = _mm512_dpbusd_epi32(_src3, _src1, _src2);
 
-			// Going over every row of the input
-			for (int k = 0; k < kernel_rows; k++)
-			{
-				// Going over every column of each row
-				for (int l = 0; l < kernel_columns; l++)
-				{
-					// Convolute input square with kernel square
-					convolute += input[x][y] * kernel[k][l];
-					y++; // Move right.
-				}
-				x++; // Move down.
-				y = j; // Restart column position
-			}
-			assert(i < width && j < height);
-			output[i][j] = convolute + bias; // Add result to output matrix.
-			convolute = 0; // Needed before we move on to the next index.
-		}
+	_mm512_storeu_epi32(convolutions, _dst);
+
+	for (int i = 0; i < width * height;i++) {
+
+		std::cout << convolutions[i] << " ";
+		if ((i + 1) % width == 0) std::cout << std::endl;
 	}
-	for (std::array<int, width> row : output) {
-		for (int element : row) {
-			std::cout << element << " ";
-		}
-		std::cout << std::endl;
-	}
+
 }
+
 
 void bench_conv() {
 
@@ -246,14 +215,14 @@ void bench_conv() {
 												-1, 5 };
 
 	convolute(A, Kernel, 5);
-
+	convoluteSIMD(A, Kernel, 5);
 	return;
 }
 
 int main()
 {
 	//bench_primitive();
-	bench_nnue();
-	//	bench_conv();
+	//bench_nnue();
+	bench_conv();
 	return 0;
 }
